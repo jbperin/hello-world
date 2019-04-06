46c6d4ac22fda413fd235116a2e0f9b19d1fec4a
jbperin <jbperin@users.noreply.github.com>
2019-04-06 02:04:31 +0200

    add puml, toc and code hilite to mdrender

--- mdrender.py
+++ mdrender.py
@@ -1,21 +1,48 @@
 
 import markdown
+from markdown.extensions.toc import TocExtension
+import codecs
 
-#input_file = codecs.open("pythonMarkdown.py", mode="r", encoding="utf-8")
-#text = input_file.read()
-text = """#Coucou
-## C'estun essai
-"""
+input_file = codecs.open("pythonMarkdown.md", mode="r", encoding="utf-8")
+text = input_file.read()
+##text = """
+##[TOC]
+###Coucou
+#### C'estun essai
+##
+##```plantuml format="png" classes="uml myDiagram" alt="My super diagram placeholder" title="My super diagram" width="300px" height="300px"
+##  Goofy ->  MickeyMouse: calls
+##  Goofy <-- MickeyMouse: responds
+##```
+##"""
+extension_configs = {
+    'toc': {
+        'marker':'[TOC]',
+        'title':'TableOfContent',
+        'baselevel': 1,
+        'permalink': True
+        
+    },
+    'plantuml_markdown': {
+    },
+    'codehilite': {
+        'linenums':False
+        ,'guess_lang':False
+        ,'css_class':'codehilite'
+        ,'pygments_style':'default' # Pygments HTML Formatter Style (ColorScheme). Defaults to default.
+        ,'noclasses':True
+        ,'use_pygments':True
+    }
+}
+html = markdown.markdown(text, extensions=["extra", "toc", "plantuml_markdown", "codehilite"], extension_configs=extension_configs)
 
-html = markdown.markdown(text)
 
 
-
-#output_file = codecs.open("pythonMarkdown.html", "w",
-#                          encoding="utf-8",
-#                          errors="xmlcharrefreplace"
-#)
-#output_file.write(html)
-#output_file.close
+output_file = codecs.open("pythonMarkdown.html", "w",
+                          encoding="utf-8",
+                          errors="xmlcharrefreplace"
+)
+output_file.write(html)
+output_file.close()
 
 print (html)
--- pythonMarkdown.md
+++ pythonMarkdown.md
@@ -1,5 +1,86 @@
 
+[TOC]
 
+# Premier pas
+
+## Installation
+
+
+```bash
+sudo apt-get install graphviz
+sudo apt-get install plantuml
 ```
+
+```bash
 pip3 install markdown
 ```
+
+Premier usage
+
+```bash
+python3 -m markdown <fichier.md>
+```
+
+Usage programmatique
+
+```python hl_lines="1 3"
+import markdown
+
+text = """#Coucou
+## C'estun essai
+"""
+
+html = markdown.markdown(text)
+
+
+print (html)
+```
+
+## Extension plantuml
+
+```bash
+pip3 install plantuml-markdown
+```
+
+```bash
+python3 -m markdown -x plantuml_markdown  pythonMarkdown.md
+```
+
+
+```plantuml format="txt" classes="uml myDiagram" alt="My super diagram placeholder" title="My super diagram" width="300px" height="300px"
+  Goofy ->  MickeyMouse: calls
+  Goofy <-- MickeyMouse: responds
+```
+
+
+```plantuml format="png" classes="uml myDiagram" alt="My super diagram placeholder" title="My super diagram" width="300px" height="300px"
+@startuml
+
+(First usecase)
+(Another usecase) as (UC2)  
+usecase UC3
+usecase (Last\nusecase) as UC4
+
+@enduml
+```
+
+```plantuml format="png" classes="uml myDiagram" alt="My super diagram placeholder" title="My super diagram" width="300px" height="300px"
+@@startdot
+digraph foo {
+  node [style=rounded]
+  node1 [shape=box]
+  node2 [fillcolor=yellow, style="rounded,filled", shape=diamond]
+  node3 [shape=record, label="{ a | b | c }"]
+
+  node1 -> node2 -> node3
+}
+@enddot
+```
+
+## Extension CodeHilite
+
+```
+sudo apt-get install python3-pygments
+pip3 install Pygments
+```
+
