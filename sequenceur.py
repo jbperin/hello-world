b70259b7ecc36ec41222a735c37a724c80e1e7c1
jbperin <jbperin@users.noreply.github.com>
2019-04-18 11:45:29 +0200

    add mqtt publishing feature

--- sequenceur.py
+++ sequenceur.py
@@ -9,8 +9,42 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
+
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+SERVER = "liveobjects.orange-business.com"
+PORT = 1883
+API_KEY   = "e36519a3c5474778a0f0d2a867843eba"
+USERNAME  = "json+device"
 CLIENT_ID = "urn:lo:nsid:flexy205:0475410668"
 
+
+#Publications parameters
+TOPIC="dev/data" # topic to publish to
+qos = 1
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+sampleClient = mqtt.Client(CLIENT_ID, clean_session=True, userdata = None, protocol=mqtt.MQTTv311, transport="tcp")
+sampleClient.on_connect = on_connect
+sampleClient.on_message = on_message
+
+sampleClient.username_pw_set(USERNAME,password = API_KEY) # use device mode and set the password
+# now connect to LO
+sampleClient.connect(SERVER, PORT, 60)
+
+sampleClient.loop_start()
 
 import sched, time
 
@@ -22,6 +56,7 @@ def schedule():
     s.enter(2, 1, publish, argument=('chaine de donnée',))
     s.enter(3, 2, publish, argument=('chaine de donnée',))
     s.run()
+
 
 import datetime
 from datetime import date
@@ -39,17 +74,21 @@ def processRow (row, offset_time ):
     msg = msg + ', "value":{'
     msg = msg + ' "variable" : "' + row[0] + '"'
     msg = msg + ', "valeur" : ' + row[2]
+    msg = msg + ', "timestamp2": ' + str(float(row[4].replace(',', '.')))  	## timestamp
     msg = msg + ', "timestamp": "' + dtformated + 'Z"';  	## timestamp
     msg = msg + '} '								## data (json format)
     msg = msg + ', "loc":[45.759723,4.84223]'       ## loc
     msg = msg + ', "t": ["MyTag"]'
     msg = msg + '}'									## tags
 
+    if row[0] in [ "ibo_Zen_A1-A2_rel_R" ]:
+        sampleClient.publish(TOPIC, msg, qos)
+        print ("%s, %f, %s"%(dtformated, offset_time, msg) )
     #msg = '{"s":"urn:lo:nsid:flexy205:0475410668!uplink", "m":"sample01", "value":{"engineOn":true,"tempC":-2.39, "ts": "2018-06-21T09:55:47Z"}, "t": ["sample.01"], "loc":[45.759723,4.84223]}'
 
     #timedt = datetime.datetime.fromordinal(timest)
     #offset_time = timest - ref_time
-    print ("%s, %f, %s"%(dtformated, offset_time, msg) ) #(', '.join(row))
+     #(', '.join(row))
 
 
 import csv
@@ -71,6 +110,9 @@ def litCSV(filepath, nb = None):
                 processRow (row, offsettime)
             nbval = nbval + 1
             if (nb != None) and (nbval > 100) : break
+
+
+
 
 if __name__ == '__main__':
 
