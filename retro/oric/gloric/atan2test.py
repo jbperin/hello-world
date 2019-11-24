31d28cc9987d077000b09594c73fd095b66922af
jbperin <jbperin@users.noreply.github.com>
2019-11-24 18:44:20 +0100

    prepare intense testing of atan2

---
+++ retro/oric/gloric/atan2test.py
@@ -0,0 +1,19 @@
+import math
+N=5
+
+with open('output.txt', 'w') as file:  # Use file to refer to the file object
+
+
+    for X in range (-N,N+1):
+        for Y in range (-N,N+1):
+            exp = round(math.atan2(Y,X)*(128.0/math.pi))
+            if (exp == 128):  exp = -128
+            file.write("""\tTanX = %d; TanY = %d; Arctan8 = 0;
+\tatan2 ();
+\tif (Arctan8!=%d) printf("ERR atan2 (%%d, %%d) = %%d exp=%d\\n", TanX, TanY, Arctan8);
+            \n"""%(X, Y
+            , exp
+            , exp
+            ))
+            #print (tmplt)
+
