2bb007a833cef1739e996d5bb923ee7a5b07b442
jbperin <jbperin@users.noreply.github.com>
2019-12-29 08:38:36 +0100

    attempts to use ca65 + starts thinking of clipping

--- retro/oric/gloric/caBuild.bat
+++ retro/oric/gloric/caBuild.bat
@@ -1,6 +1,6 @@
 
 ca65 -tatmos glOric.asm -o glOric.o  --include-dir .
-cl65 -tatmos demo.c glOric.o -o toto.tap
+cl65 -C linker.cfg  -tatmos demo.c glOric.o -o toto.tap
 copy toto.tap C:\Users\Public\osdk_1_15\Oricutron\tapes
 
 PUSHD C:\Users\Public\osdk_1_15\Oricutron
