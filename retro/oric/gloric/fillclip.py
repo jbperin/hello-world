334f1db8cb1b3b35c36759c7cfd6b585fc32dab6
jbperin <jbperin@users.noreply.github.com>
2019-12-30 22:33:35 +0100

    a bit further on the way to polygon clipping

--- retro/oric/gloric/fillclip.py
+++ retro/oric/gloric/fillclip.py
@@ -32,7 +32,7 @@ class bres_agent :
             self.sy = 1
         else:
             self.sy = -1
-        self.arrived = (self.X != self.destX) or ( self.Y != self.destY)
+        self.arrived = (self.X == self.destX) and ( self.Y == self.destY)
 
     def step(self,):
 
@@ -51,6 +51,25 @@ class bres_agent :
         return retval
 
 
+    def stepY(self,):
+        nxtY = self.Y+self.sy
+        e2 = 2*self.err;
+        while ( not self.arrived and ((e2 > self.dX ) or (self.Y != nxtY))):
+            if (e2 >= self.dY):
+                self.err += self.dY; # e_xy+e_x > 0 */
+                self.X += self.sx;
+            if (e2 <= self.dX): # e_xy+e_y < 0 */
+                self.err += self.dX;
+                self.Y += self.sy;
+    
+            if (self.X == self.destX) and ( self.Y == self.destY):
+                self.arrived = True
+            e2 = 2*self.err;
+            
+        retval = [self.X, self.Y]
+        return retval
+
+
 def main():
     pDep = [1,5]
     pArr1 = [3,1]
@@ -58,23 +77,29 @@ def main():
     a1 = bres_agent(pDep[0],pDep[1],pArr1[0],pArr1[1])
     a2 = bres_agent(pDep[0],pDep[1],pArr2[0],pArr2[1])
 
-    [a1x, a1y] = [a1.X, a1.Y]
-    [a2x, a2y] = [a2.X, a2.Y]
-    while (a1.Y == a1y): a1.step()
-    while (a2.Y == a2y): a2.step()
-    print ([a1.X, a1.Y], [a2.X,a2.Y])
+    print (a1.stepY(), a2.stepY())
+    print (a1.stepY(), a2.stepY())
+    if (a2.arrived):
+        a2 = bres_agent(pArr2[0],pArr2[1],pArr1[0],pArr1[1])
+    print (a1.stepY(), a2.stepY())
+    print (a1.stepY(), a2.stepY())
+    # [a1x, a1y] = [a1.X, a1.Y]
+    # [a2x, a2y] = [a2.X, a2.Y]
+    # while (a1.Y == a1y): a1.step()
+    # while (a2.Y == a2y): a2.step()
+    # print ([a1.X, a1.Y], [a2.X,a2.Y])
 
-    [a1x, a1y] = [a1.X, a1.Y]
-    [a2x, a2y] = [a2.X, a2.Y]
-    while (a1.Y == a1y): a1.step()
-    while (a2.Y == a2y): a2.step()
-    print ([a1.X, a1.Y], [a2.X,a2.Y])
+   ##   [a1x, a1y] = [a1.X, a1.Y]
+    # [a2x, a2y] = [a2.X, a2.Y]
+    # while (a1.Y == a1y): a1.step()
+    # while (a2.Y == a2y): a2.step()
+    # print ([a1.X, a1.Y], [a2.X,a2.Y])
 
-    [a1x, a1y] = [a1.X, a1.Y]
-    [a2x, a2y] = [a2.X, a2.Y]
-    while (a1.Y == a1y): a1.step()
-    while (a2.Y == a2y): a2.step()
-    print ([a1.X, a1.Y], [a2.X,a2.Y])
+   ##   [a1x, a1y] = [a1.X, a1.Y]
+    # [a2x, a2y] = [a2.X, a2.Y]
+    # while (a1.Y == a1y): a1.step()
+    # while (a2.Y == a2y): a2.step()
+    # print ([a1.X, a1.Y], [a2.X,a2.Y])
 
     #print (a1.step())
     #print (a1.step())
