0e455dcc4b57853e994b4e29cff30b7f00492d99
jbperin <jbperin@users.noreply.github.com>
2020-02-17 20:20:02 +0100

    tool for memory adress computation

--- retro/oric/labo/compaddresses.py
+++ retro/oric/labo/compaddresses.py
@@ -3,8 +3,10 @@
 
 LORES_SCREEN_ADDRESS= 0xBB80
 HIRES_SCREEN_ADDRESS= 0xA000
+
 LORES_SCREEN_WIDTH= 40
 LORES_SCREEN_HEIGHT= 26
+
 HIRES_SCREEN_WIDTH= 240
 HIRES_SCREEN_HEIGHT= 200
 
@@ -43,17 +45,32 @@ TEXT_50Hz = 26
 HIRES_60Hz = 28
 HIRES_50Hz = 30
 
-STD_CHARSET_ADDRESS = 0xB400
-ALT_CHARSET_ADDRESS = 0xB800
-NB_STD_CHAR = 128
-NB_ALT_CHAR = 112
+STD_LORES_CHARSET_ADDRESS = 0xB400
+ALT_LORES_CHARSET_ADDRESS = 0xB800
+
+STD_HIRES_CHARSET_ADDRESS = 0x9800
+ALT_HIRES_CHARSET_ADDRESS = 0x9C00
+
+NB_STD_LORES_CHAR = 128
+NB_ALT_LORES_CHAR = 112
+
+NB_STD_HIRES_CHAR = 128
+NB_ALT_HIRES_CHAR = 128
+
 NB_BYTES_PER_CHAR = 8
 
-def isInStdCharset(addr):
-    return (addr >= STD_CHARSET_ADDRESS) and (addr < STD_CHARSET_ADDRESS + NB_STD_CHAR*NB_BYTES_PER_CHAR)
 
-def isInAltCharset(addr):
-    return (addr >= ALT_CHARSET_ADDRESS) and (addr < ALT_CHARSET_ADDRESS + NB_ALT_CHAR*NB_BYTES_PER_CHAR)
+def isInStdLoresCharset(addr):
+    return (addr >= STD_LORES_CHARSET_ADDRESS) and (addr < STD_LORES_CHARSET_ADDRESS + NB_STD_LORES_CHAR*NB_BYTES_PER_CHAR)
+
+def isInAltLoresCharset(addr):
+    return (addr >= ALT_LORES_CHARSET_ADDRESS) and (addr < ALT_LORES_CHARSET_ADDRESS + NB_ALT_LORES_CHAR*NB_BYTES_PER_CHAR)
+
+def isInStdHiresCharset(addr):
+    return (addr >= STD_HIRES_CHARSET_ADDRESS) and (addr < STD_HIRES_CHARSET_ADDRESS + NB_STD_HIRES_CHAR*NB_BYTES_PER_CHAR)
+
+def isInAltHiresCharset(addr):
+    return (addr >= ALT_HIRES_CHARSET_ADDRESS) and (addr < ALT_HIRES_CHARSET_ADDRESS + NB_ALT_HIRES_CHAR*NB_BYTES_PER_CHAR)
 
 def isInLoresMemory(addr):
     return (addr >= LORES_SCREEN_ADDRESS) and (addr < LORES_SCREEN_ADDRESS + LORES_SCREEN_WIDTH*LORES_SCREEN_HEIGHT)
@@ -62,32 +79,30 @@ def isInHiresMemory(addr):
     return (addr >= HIRES_SCREEN_ADDRESS) and (addr < HIRES_SCREEN_ADDRESS + LORES_SCREEN_WIDTH*HIRES_SCREEN_HEIGHT+40+2*LORES_SCREEN_WIDTH)
 
 def asciiCodeOfCharAt(addr):
-    if isInStdCharset(addr):
-        return "STD code = %d, line=%d"%((addr-STD_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-STD_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
-    if isInAltCharset(addr):
-        return "ALT code = %d, line=%d"%((addr-ALT_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-ALT_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
-
-def coordsOfCharAt(addr):
-    if isInLoresMemory(addr):
-        return "STD code = %d, line=%d"%((addr-STD_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-STD_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
-    if isInHiresMemory(addr):
-        return "ALT code = %d, line=%d"%((addr-ALT_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-ALT_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
+    if isInStdLoresCharset(addr):
+        return "LORES STD code = %d, line=%d"%((addr-STD_LORES_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-STD_LORES_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
+    if isInAltLoresCharset(addr):
+        return "LORES ALT code = %d, line=%d"%((addr-ALT_LORES_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-ALT_LORES_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
+    if isInStdHiresCharset(addr):
+        return "HIRES STD code = %d, line=%d"%((addr-STD_HIRES_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-STD_HIRES_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
+    if isInAltHiresCharset(addr):
+        return "HiRES ALT code = %d, line=%d"%((addr-ALT_HIRES_CHARSET_ADDRESS)/NB_BYTES_PER_CHAR, (addr-ALT_HIRES_CHARSET_ADDRESS)%NB_BYTES_PER_CHAR)
 
 def write_screen_memory():
 
     print("LORES ADDR:")
     for ii in range(LORES_SCREEN_HEIGHT):
         addr = LORES_SCREEN_ADDRESS+(ii*LORES_SCREEN_WIDTH)+0
-        if (isInStdCharset(addr) or isInAltCharset(addr)):
+        if (isInStdLoresCharset(addr) or isInAltLoresCharset(addr)):
             print(ii, "%x"%addr, asciiCodeOfCharAt(addr))
 
     print("HIRES ADDR:")
     for ii in range (HIRES_SCREEN_HEIGHT):
         addr1 = HIRES_SCREEN_ADDRESS+(ii*LORES_SCREEN_WIDTH)+1
-        if (isInStdCharset(addr1) or isInAltCharset(addr1)):
+        if (isInStdLoresCharset(addr1) or isInAltLoresCharset(addr1)):
             print(ii, "%x"%addr1, asciiCodeOfCharAt(addr1))
         addr2 = HIRES_SCREEN_ADDRESS+(ii*LORES_SCREEN_WIDTH)+2
-        if (isInStdCharset(addr2) or isInAltCharset(addr2)):
+        if (isInStdLoresCharset(addr2) or isInAltLoresCharset(addr2)):
             print(ii, "%x"%addr2, asciiCodeOfCharAt(addr2))
 
 
