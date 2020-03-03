d1b8d4945ba32aa4da7c029e5b84ea341280aef1
jbperin <jbperin@users.noreply.github.com>
2020-03-03 23:06:43 +0100

    sound does not work

--- retro/oric/soundlabo/main.c
+++ retro/oric/soundlabo/main.c
@@ -10,39 +10,65 @@
 
 // Declare the assembly code function
 void SimplePrint(const char *ptr_message);
+// SimplePrint("Hello World !");
 
 void write2AyChip (unsigned char regnumber, unsigned char regvalue);
+
+// int play(int soundchanels,int noisechanels,int envelop,int volume);
+// int music(int chanel,int octave,int key,int volume);
+// int sound(int chanel,int period,int volume);
 
 void main()
 {
 	// char valeur[220];
-	// int val ;
-	SimplePrint("Hello World !");
+	int val ;
+	
+	int soundchanels, noisechanels, envelop, volume;
+	int chanel, octave, period, key;
 
+	// soundchanels = ; noisechanels = ; envelop = ; volume = ;
+	// play(soundchanels,noisechanels,envelop, volume);
 
-		write2AyChip (0, 0);
-		write2AyChip (1, 0);
-		write2AyChip (2, 0);
-		write2AyChip (3, 0);
-		write2AyChip (4, 0);
-		write2AyChip (5, 0);
-		write2AyChip (8, 0);
-		write2AyChip (9, 0);
-		write2AyChip (13, 0);
+	// chanel= ; octave= ; key= ; volume=;
+	// music( chanel, octave, key, volume);
+
+	// sound(chanel, period, volume);
+	// chanel  = 1;
+	// volume = 5;
+	// for (octave = 0 ; octave <= 6; octave ++) {
+	// 	for (key = 1 ; key <= 12; key ++){
+	// 		music( chanel, octave, key, volume);
+	// 		for (val =0; val< 6500; val++);
+	// 	}
+	// }
+	// play(12,0,0,0);
+
+	music( 2, 2, 5, 5);
+	for (val =0; val< 6500; val++);
+	play (0, 0, 0, 0);
+		// write2AyChip (0, 0);
+		// write2AyChip (1, 0);
+		// write2AyChip (2, 0);
+		// write2AyChip (3, 0);
+		// write2AyChip (4, 0);
+		// write2AyChip (5, 0);
+		// write2AyChip (8, 0);
+		// write2AyChip (9, 0);
+		// write2AyChip (13, 0);
 
 	// write2AyChip (0x0D, 0x08);
-	while (1==1) {
-		get();
-		printf ("son \n");
+	// while (1==1) {
+	// 	get();
+	// 	printf ("son \n");
 
-		write2AyChip (6, 17);
-		write2AyChip (7, 7);
-		write2AyChip (10, 20);
-		// write2AyChip (11, 20);
-		// write2AyChip (12, 20);
-		// write2AyChip (14, 20);
-		// write2AyChip (15, 0);
-	}
+	// 	// write2AyChip (6, 17);
+	// 	// write2AyChip (7, 7);
+	// 	// write2AyChip (10, 20);
+	// 	// write2AyChip (11, 20);
+	// 	// write2AyChip (12, 20);
+	// 	// write2AyChip (14, 20);
+	// 	// write2AyChip (15, 0);
+	// }
 
 	// scanf ("%d", &val);
 
--- retro/oric/soundlabo/sound.s
+++ retro/oric/soundlabo/sound.s
@@ -30,17 +30,21 @@ write2ay:
 // void write2AyChip (unsigned char regnumber, unsigned char regvalue);
 _write2AyChip:
 .(
+	ldy #2
+	lda (sp),y
+	TAX
+
 	ldy #0
 	lda (sp),y
-	sta AYRegisterNumber
-	iny
-	lda (sp),y
-	sta AYRegisterValue
 
-    jsr write2ay
+    jsr WriteToAY
 .)    
     rts
 
+#define VIA_PCR $30C
+#define VIA_IORA2 $30F
+; parametre A = register number of 8912
+; parametre X = donnée à transférer
 WriteToAY:
 .(
     PHP              
@@ -49,8 +53,9 @@ WriteToAY:
     TAY              
     TXA              
     CPY #$07         
-    BNE $F59D        
+    BNE WriteToAY_F59D        
     ORA #$40         
+WriteToAY_F59D:
     PHA              
     LDA VIA_PCR      
     ORA #$EE         
@@ -67,6 +72,165 @@ WriteToAY:
     AND #$11         
     ORA #$CC         
     STA VIA_PCR      
-    PLP              
+    plp
+.)              
+    rts              
+
+unknown: // F2E4
+.(
+  STA $0204        
+  LDA $0201,X      
+  BNE unknown_F2F6        
+  LDA $0200,X      
+  BEQ unknown_F2F6       
+  CMP $0204        
+  BCC unknown_F2F7        
+unknown_F2F6:
+  SEC
+unknown_F2F7:
+.)  
+	RTS   
+
+unknown2: // F2F8
+.(
+    STA $0204        
+    LDA $0201,X      
+    BNE unknown2_F308        
+    LDA $0200,X      
+    CMP $0204        
+    BCC unknown2_F309        
+unknown2_F308: 
+    SEC              
+unknown2_F309: 
 .)
-    RTS            
+RTS      
+
+_canal .dsb 1  // PARAMS + 1
+_music_octave .dsb 1 // PARAMS + 3
+_music_note .dsb 1   // PARAMS + 5
+_music_volume .dsb 1 // PARAMS + 7
+
+// int myMusic(int chanel,int octave,int key,int volume)
+_myMusic:
+.(
+    ldy #0:	lda (sp),y: sta _canal
+	ldy #2:	lda (sp),y: sta _music_octave
+    ldy #4:	lda (sp),y: sta _music_note
+    ldy #6:	lda (sp),y: sta _music_volume
+	jsr MUSIC
+
+.)
+    rts
+MUSIC:
+.(
+    LDX #$E1         
+    LDA #$04         
+    JSR unknown        
+    BCS MUSIC_FC5A        
+    LDX #$E3         
+    LDA #$08         
+    JSR unknown2        
+    BCS MUSIC_FC5A        
+    LDX #$E5         
+    LDA #$0D         
+    JSR unknown        
+    BCS MUSIC_FC5A        
+    LDY _music_octave // $02E3        
+    LDX _music_note // $02E5        
+    LDA MusicData,X  
+    STA snd_period+1        
+    LDA MUSIC_FC6B,X      
+    STA snd_period        
+    LDA _music_volume        
+    STA snd_volume        
+MUSIC_FC4B:      DEY              
+    BMI MUSIC_FC57        
+    LSR snd_period+1        
+    ROR snd_period        
+    JMP MUSIC_FC4B        
+MUSIC_FC57:  JMP SOUND        
+MUSIC_FC5A:  ;INC $02E0  
+.)      
+    RTS 
+
+MusicData: .byt $00, $07, $07, $06, $06, $05, $05, $05, $04, $04, $04, $04
+MUSIC_FC6B: .byt $00, $77, $0B, $A6, $47, $EC, $97, $47, $FB, $B3, $70, $30 
+
+snd_period .dsb 2   // $02E3
+snd_volume .dsb 1   // $02E5
+
+SOUND: // FB40
+.(
+    LDA _canal // $02E1        
+    CMP #$01         
+    BNE SOUND_FB69        
+    LDA #$00         
+    LDX snd_period // $02E3        
+    JSR WriteToAY    
+    LDA #$01         
+    LDX snd_period+1    // $02E4     
+    JSR WriteToAY    
+SOUND_FB57:  
+    LDA snd_volume // $02E5        
+    AND #$0F         
+    BNE SOUND_FB62        
+    LDX #$10         
+    BNE SOUND_FB63        
+SOUND_FB62:  TAX              
+SOUND_FB63: LDA #$08         
+    JSR WriteToAY    
+    RTS              
+SOUND_FB69: CMP #$02         
+    BNE SOUND_FB8F        
+    LDA #$02         
+    LDX snd_period // $02E3        
+    JSR WriteToAY    
+    LDA #$03         
+    LDX snd_period+1    // $02E4        
+    JSR WriteToAY    
+SOUND_FB7D: 
+    LDA snd_volume // $02E5        
+    AND #$0F         
+    BNE SOUND_FB88        
+    LDX #$10         
+    BNE SOUND_FB89        
+SOUND_FB88: 
+    TAX              
+SOUND_FB89: 
+    LDA #$09         
+    JSR WriteToAY    
+    RTS              
+SOUND_FB8F: 
+    CMP #$03         
+    BNE SOUND_FBB5        
+    LDA #$04         
+    LDX snd_period // $02E3        
+    JSR WriteToAY    
+    LDA #$05         
+    LDX snd_period+1    // $02E4        
+    JSR WriteToAY    
+SOUND_FBA3: 
+    LDA snd_volume // $02E5        
+    AND #$0F         
+    BNE SOUND_FBAE        
+    LDX #$10         
+    BNE SOUND_FBAF        
+SOUND_FBAE: 
+    TAX              
+SOUND_FBAF:
+    LDA #$0A         
+    JSR WriteToAY    
+    RTS              
+SOUND_FBB5: LDA #$06         
+    LDX snd_period // $02E3        
+    JSR WriteToAY    
+    LDA _canal // $02E1        
+    CMP #$04         
+    BEQ SOUND_FB57        
+    CMP #$05         
+    BEQ SOUND_FB7D        
+    CMP #$06         
+    BEQ SOUND_FBA3        
+    ;INC $02E0        
+.)
+    RTS              
