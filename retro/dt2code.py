08fe5dff1710155bb100a3fdbca9ab2fa74f868a
Jean-Baptiste <jean-baptiste.perin@orange.com>
2025-08-12 16:02:46 +0200

    recursively build a nice, light and clean abstract tree for forthcoming code generation

--- retro/dt2code.py
+++ retro/dt2code.py
@@ -1,5 +1,3 @@
-
-
 # recherche r3:
 # 	r3 = 0
 
@@ -50,6 +48,9 @@ import math
 from sympy.logic import POSform, SOPform
 from sympy import symbols
 
+import json 
+
+
 symA = 'a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15'
 symB = 'b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 b10 b11 b12 b13 b14 b15'
 
@@ -63,58 +64,7 @@ B = [b0, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15]
 def toBin (val, size):
     return list(reversed(list(map(lambda x: int(x),'{:0{}b}'.format(val, size)))))
 
-def runthrough(clf):
-    n_nodes = clf.tree_.node_count
-    children_left = clf.tree_.children_left
-    children_right = clf.tree_.children_right
-    feature = clf.tree_.feature
-    threshold = clf.tree_.threshold
-    values = clf.tree_.value
-
-    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
-    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
-    stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
-    while len(stack) > 0:
-        # `pop` ensures each node is only visited once
-        node_id, depth = stack.pop()
-        node_depth[node_id] = depth
-
-        # If the left and right child of a node is not the same we have a split
-        # node
-        is_split_node = children_left[node_id] != children_right[node_id]
-        # If a split node, append left and right children and depth to `stack`
-        # so we can loop through them
-        if is_split_node:
-            stack.append((children_left[node_id], depth + 1))
-            stack.append((children_right[node_id], depth + 1))
-        else:
-            is_leaves[node_id] = True
-
-    print(
-        "The binary tree structure has {n} nodes and has "
-        "the following tree structure:\n".format(n=n_nodes)
-    )
-    for i in range(n_nodes):
-        if is_leaves[i]:
-            print(
-                "{space}node={node} is a leaf node with value={value}.".format(
-                    space=node_depth[i] * "\t", node=i, value=values[i]
-                )
-            )
-        else:
-            print(
-                "{space}node={node} is a split node with value={value}: "
-                "go to node {left} if X[:, {feature}] <= {threshold} "
-                "else to node {right}.".format(
-                    space=node_depth[i] * "\t",
-                    node=i,
-                    left=children_left[i],
-                    feature=feature[i],
-                    threshold=threshold[i],
-                    right=children_right[i],
-                    value=values[i],
-                )
-            )
+NBITS = 4
 
 
 def build_X_y(NBITS, idxOfBitToEncode, hypothesis):
@@ -132,108 +82,63 @@ def build_X_y(NBITS, idxOfBitToEncode, hypothesis):
         mathval1 = round(math.sqrt(v1))
         binval1 = toBin(mathval1, NBITS)
         targetbit = binval1[idxOfBitToEncode]
-        print(v1, binv1, mathval1, binval1, targetbit)
+        # print(v1, binv1, mathval1, binval1, targetbit)
         X.append(binv1)
         y.append(targetbit)
-    print(X)
-    print(y)
+    # print(X)
+    # print(y)
     return X, y
 
-
-NBITS = 4
-idxOfBitToEncode = 3
-hypothesis = []  # Example hypothesis to filter inputs
-
-X, y = build_X_y(NBITS, idxOfBitToEncode, hypothesis)
+def display_tree(tree):
+    r = export_text(tree) # , feature_names = ["a", "b"])
+    print (r)
 
 
-print (X)
-print(y)
-
-tree = DecisionTreeClassifier(max_depth=2).fit(X, y)
-
-# r = export_text(tree, feature_names = symA.split()[0:NBITS]) # , feature_names = ["a", "b"])
-
-# print (r)
-
-node_id         = 0
-children_left   = tree.tree_.children_left
-children_right  = tree.tree_.children_right
-feature         = tree.tree_.feature
-threshold       = tree.tree_.threshold
-values          = tree.tree_.value
-
-# n_nodes         = tree.tree_.node_count
-is_split_node = children_left[node_id] != children_right[node_id]
-
-if not is_split_node:
-    print(
-        "{space}node={node} is a leaf node with value={value}.".format(
-            space=1 * "\t", node=node_id, value=values[node_id]
-        )
-    )
-else:
-    print(
-        "{space}node={node} is a split node with value={value}: "
-        "go to node {left} if X[:, {feature}] <= {threshold} "
-        "else to node {right}.".format(
-            space=1 * "\t",
-            node=node_id,
-            left=children_left[node_id],
-            feature=feature[node_id],
-            threshold=threshold[node_id],
-            right=children_right[node_id],
-            value=values[node_id],
-        )
-    )
-
-        
-# runthrough(tree)
 
 
-idxOfBitToEncode = 2
-hypothesis = []  # Example hypothesis to filter inputs
+def full_abstract_tree   (listOfIdxOfBitToEncode,  hypothesis):
 
-X, y = build_X_y(NBITS, idxOfBitToEncode, hypothesis)
+    idxOfBitToEncode = listOfIdxOfBitToEncode[0]
+
+    X, y = build_X_y(NBITS, idxOfBitToEncode, hypothesis)
+
+    tree = DecisionTreeClassifier().fit(X, y) # max_depth=2
+    display_tree(tree)
+    node_id=0
+
+    if tree.tree_.children_left[node_id] == tree.tree_.children_right[node_id]:  # Leaf node
+        listOfIdxOfBitToEncode.pop(0)  
+        max_idx = np.argmax(tree.tree_.value[node_id][0].tolist())          
+        if len(listOfIdxOfBitToEncode) != 0:                
+            result = {'value': f"r{idxOfBitToEncode} = {max_idx}",
+                        'subtree': full_abstract_tree(listOfIdxOfBitToEncode, hypothesis)
+                        }
+            # listOfIdxOfBitToEncode.insert(0, sav)  # Restore the index for the next call
+            return result
+        else:
+            result = {'value': f"r{idxOfBitToEncode} = {y[0]}"}
+            # listOfIdxOfBitToEncode.insert(0, sav)
+            return result
+    else:
+        result = {
+            'feature': tree.tree_.feature[node_id],
+            # 'threshold': "", #tree.tree_.threshold[node_id], #
+            # 'value': "", # tree.tree_.value[node_id][0].tolist(), # 
+        }
+        savl = listOfIdxOfBitToEncode.copy()
+        result['left'] = full_abstract_tree(listOfIdxOfBitToEncode, hypothesis + [(tree.tree_.feature[node_id], 0)])
+        listOfIdxOfBitToEncode = savl.copy()
+        result['right'] = full_abstract_tree(listOfIdxOfBitToEncode, hypothesis + [(tree.tree_.feature[node_id], 1)])
+        listOfIdxOfBitToEncode = savl.copy()
+
+        return result
+    
+listOfIdxOfBitToEncode = [3, 2, 1, 0]  
+hypothesis = []
+
+abstree = full_abstract_tree   (listOfIdxOfBitToEncode,  hypothesis)
 
 
-print (X)
-print(y)
+print(json.dumps(json.loads(str(abstree).replace ("'",'"')), indent=2))
 
-tree = DecisionTreeClassifier(max_depth=2).fit(X, y)
-
-# r = export_text(tree, feature_names = symA.split()[0:NBITS]) # , feature_names = ["a", "b"])
-
-# print (r)
-
-node_id         = 0
-children_left   = tree.tree_.children_left
-children_right  = tree.tree_.children_right
-feature         = tree.tree_.feature
-threshold       = tree.tree_.threshold
-values          = tree.tree_.value
-
-# n_nodes         = tree.tree_.node_count
-is_split_node = children_left[node_id] != children_right[node_id]
-
-if not is_split_node:
-    print(
-        "{space}node={node} is a leaf node with value={value}.".format(
-            space=1 * "\t", node=node_id, value=values[node_id]
-        )
-    )
-else:
-    print(
-        "{space}node={node} is a split node with value={value}: "
-        "go to node {left} if X[:, {feature}] <= {threshold} "
-        "else to node {right}.".format(
-            space=1 * "\t",
-            node=node_id,
-            left=children_left[node_id],
-            feature=feature[node_id],
-            threshold=threshold[node_id],
-            right=children_right[node_id],
-            value=values[node_id],
-        )
-    )
 
